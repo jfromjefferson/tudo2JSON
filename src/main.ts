3e46ff0eed881810a4865208d3443bc692228e60
Jefferson Silva <jeffsilva1@outlook.com>
2022-10-04 21:28:15 -0300

    Add styles, show recipe list

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,50 @@
 import { htmlParser } from './parser';
 import { getRecipe, getRecipeUrlList } from './utils/utils';
-
-// const tempUrl = 'https://www.tudogostoso.com.br/categorias/1004-carnes';
-const tempUrl = 'https://www.tudogostoso.com.br/receita/47884-esfiha-de-carne-adaptada-receita-turca.html';
+import './styles.scss';
 
 (async () => {
-  const page = await htmlParser(tempUrl);
 
-  if(tempUrl.split('categorias').length === 1){
-    const recipe = getRecipe(page)
+  document.querySelector('form')?.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const get_recipe_button = document.querySelector('.get-recipe-button')!;
 
-    console.log(recipe)
+    get_recipe_button.classList.add('loading');
+    get_recipe_button.setAttribute('disabled', 'disabled');
 
-  }else {
-    const recipeUrlList = getRecipeUrlList(page)
+    document.querySelector('body')?.classList.add('loading');
 
-    const recipeList = await Promise.all(recipeUrlList.map(async url => {
-      const recipePage = await htmlParser(url);
-      const recipe = getRecipe(recipePage);
+    const recipeUrl = (document.querySelector('.input-recipe-url') as HTMLInputElement).value;
 
-      return recipe;
-    }));
+    const page = await htmlParser(recipeUrl);
 
-    console.log(recipeList);
-  }
+    if (recipeUrl.split('categorias').length === 1) {
+      const recipe = getRecipe(page)
 
-  
+      document.querySelector('.json-area')!.textContent = JSON.stringify(recipe, undefined, 2);
+
+      if(recipe.title) {
+        document.querySelector('.info-area p')!.textContent = '1 receita'
+      }
+
+    } else {
+      const recipeUrlList = getRecipeUrlList(page)
+
+      const recipeList = await Promise.all(recipeUrlList.map(async url => {
+        const recipePage = await htmlParser(url);
+        const recipe = getRecipe(recipePage);
+
+        return recipe;
+      }));
+
+      document.querySelector('.json-area')!.textContent = JSON.stringify(recipeList, undefined, 2);
+      document.querySelector('.info-area p')!.textContent = `${recipeList.length} receitas`
+    }
+
+    get_recipe_button.removeAttribute('disabled');
+    get_recipe_button.classList.remove('loading');
+    document.querySelector('body')?.classList.remove('loading');
+  })
+
+
 })()
 
--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,13 +1,17 @@
 
 export async function htmlParser(url: string) {
-    const response = await fetch(url);
     
     try {
+        const response = await fetch(url);
+
         const parser = new DOMParser();
         const parsed = parser.parseFromString(await response.text(), 'text/html');
 
         return parsed;
     } catch (error) {
+        alert('Can not parse html response')
+        document.querySelector('.get-recipe-button')!.removeAttribute('disabled')
+
         throw new Error(`Can not parse html response: ${error}`)
     }
 }
